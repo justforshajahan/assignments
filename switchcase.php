c5ec8f2a75cd8bb21ce0292e401973133b7ba45d
justforshajahan <shajahan.m567@gmail.com>
2017-04-02 07:58:07 +0530

    Create switchcase.php

---
+++ switchcase.php
@@ -0,0 +1,107 @@
+<?php
+$option = "6b";
+switch ($option) {
+    case "6b":
+       echo "The Selected Option Is => 6b<br>";
+	   echo "I’m 6 b!<br>";
+        break;
+    case "6bz":
+        echo "The Selected Option Is => 6bz<br>";	
+        echo "I’m 6az!<br>";
+        break;
+    case '"6"':
+	    echo "The Selected Option Is => '6'<br>";
+        echo "I’m Integer 6!<br>";
+        break;
+    case "6":
+        echo "The Selected Option Is => 6<br>"; 	
+        echo "Integer 6!<br>";
+        break;
+	default: 
+        echo "By Default<br>I’m no where!<br>";
+		}
+$option = "6bz";
+switch ($option) {
+    case "6b":
+       echo "The Selected Option Is => 6b<br>";
+	   echo "I’m 6 b!<br>";
+        break;
+    case "6bz":
+        echo "The Selected Option Is => 6bz<br>";	
+        echo "I’m 6az!<br>";
+        break;
+    case '"6"':
+	    echo "The Selected Option Is => '6'<br>";
+        echo "I’m Integer 6!<br>";
+        break;
+    case "6":
+        echo "The Selected Option Is => 6<br>"; 	
+        echo "Integer 6!<br>";
+        break;
+	default:
+        echo "By Default<br>I’m no where!<br>";
+		}		
+		$option = '"6"';
+switch ($option) {
+    case "6b":
+       echo "The Selected Option Is => 6b<br>";
+	   echo "I’m 6 b!<br>";
+        break;
+    case "6bz":
+        echo "The Selected Option Is => 6bz<br>";	
+        echo "I’m 6az!<br>";
+        break;
+    case '"6"':
+	    echo "The Selected Option Is => '6'<br>";
+        echo "I’m Integer 6!<br>";
+        break;
+    case "6":
+        echo "The Selected Option Is => 6<br>"; 	
+        echo "Integer 6!<br>";
+        break;
+	default:
+        echo "By Default<br>I’m no where!<br>";
+		}
+		$option = "6";
+switch ($option) {
+    case "6b":
+       echo "The Selected Option Is => 6b<br>";
+	   echo "I’m 6 b!<br>";
+        break;
+    case "6bz":
+        echo "The Selected Option Is => 6bz<br>";	
+        echo "I’m 6az!<br>";
+        break;
+    case '"6"':
+	    echo "The Selected Option Is => '6'<br>";
+        echo "I’m Integer 6!<br>";
+        break;
+    case "6":
+        echo "The Selected Option Is => 6<br>"; 	
+        echo "Integer 6!<br>";
+        break;
+	default:
+        echo "By Default<br>I’m no where!<br>";
+		}
+		$option = "";
+switch ($option) {
+    case "6b":
+       echo "The Selected Option Is => 6b<br>";
+	   echo "I’m 6 b!<br>";
+        break;
+    case "6bz":
+        echo "The Selected Option Is => 6bz<br>";	
+        echo "I’m 6az!<br>";
+        break;
+    case '"6"':
+	    echo "The Selected Option Is => '6'<br>";
+        echo "I’m Integer 6!<br>";
+        break;
+    case "6":
+        echo "The Selected Option Is => 6<br>"; 	
+        echo "Integer 6!<br>";
+        break;
+	default:
+        echo "By Default<br>I’m no where!<br>";
+		}
+?>
